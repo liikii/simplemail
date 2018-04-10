6447515c5e6cd72f66983a03e4a58885350ba5da
liikii <609957891@qq.com>
2018-04-10 16:57:01 +0800

    up py file

--- smtpexample.py
+++ smtpexample.py
@@ -5,36 +5,41 @@ smtp example.
 from smtplib import SMTP_SSL
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.header import Header
 
 
-def sent_mail(s, f, t, p):
+def sent_mail(s, f, t, p, sbj, txt_msg, html_msg, cc=None):
     """
     :param s: SMTP server host.
-    :param f: from.
-    :param t: to.
+    :param f: from mail.
+    :param t: to mail address list.
     :param p: password.
+    :param cc: Carbon copy;
+    :param sbj: subject.
+    :param txt_msg: plain text message.
+    :param html_msg: html message.
     """
     msg = MIMEMultipart('related')
 
-    msg['Subject'] = "thinks"
+    msg['Subject'] = Header(sbj, 'utf8', header_name='Subject').encode()
     msg['From'] = f
-    msg['To'] = t
+    msg['To'] = ','.join(t)
+    if cc:
+        msg['Cc'] = ','.join(cc)
     msg.preamble = 'This is a multi-part message in MIME format.'
 
     alt_msg = MIMEMultipart('alternative')
     msg.attach(alt_msg)
 
-    msg_plain = MIMEText('This is the alternative plain text message.')
+    msg_plain = MIMEText(txt_msg, _charset='utf-8')
     alt_msg.attach(msg_plain)
-
-    msg_html = MIMEText('<b>Some <i>HTML</i> text</b><a href="http://www.runoob.com">it\'s a link</a>', 'html')
+    msg_html = MIMEText(html_msg, 'html', 'utf-8')
     alt_msg.attach(msg_html)
 
     m = SMTP_SSL(s)
     m.login(f, p)
-    m.sendmail(f, [t], msg.as_string())
+    ts = t + cc if cc else t
+    m.sendmail(f, ts, msg.as_string())
     m.quit()
     print('ok')
 
-
-sent_mail('smtp.afghanistan.com', "mail@afghanistan.com", "tomail@afghanistan.com", 'mail')
